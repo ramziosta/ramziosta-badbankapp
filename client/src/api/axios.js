6445e2f42f432876547f2851a4dd82b011175aec
Ramzi Osta <76601606+ramziosta@users.noreply.github.com>
2022-05-25 00:59:57 -0400

    heroku test

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = `${window.location.hostname}:4000`;
+const BASE_URL = '${window.location.hostname}:4000';
 
 export default axios.create({
     baseURL: BASE_URL
