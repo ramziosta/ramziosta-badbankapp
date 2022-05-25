11857aaa42a46641762ac4aa5a7552b4fa2aea84
Ramzi Osta <76601606+ramziosta@users.noreply.github.com>
2022-05-25 00:40:49 -0400

    heroku test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
-//const corsOptions = require('./config/corsOptions')
+const corsOptions = require('./config/corsOptions')
 const cors = require("cors");
 const { logger } = require("./middleware/logEvents");
 const errorHandler = require("./middleware/errorHandler");
